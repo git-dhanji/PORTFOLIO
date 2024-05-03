b391d72713440b4563cac06a8f3a7dea9012f9aa
dhanji-kumar <kdhanji183@gmail.com>
2024-05-03 20:05:34 +0530

    update: commit for recreate from scratch

--- src/Containers/About/About.js
+++ src/Containers/About/About.js
@@ -4,7 +4,8 @@ import './about.css'
 
 export default function About() {
   return (
-    <section id='About' className='about-section'>
+    <section id='About' >
+      <div className="max-width about-section">
       <div className="about-image">
         <img src={aboutme} alt="aboutmeimag" />
       </div>
@@ -13,6 +14,7 @@ export default function About() {
         <h2>A dedicated Front-end Developer
           based in Bihar, Sasaram 📍</h2>
           <p>As a Junior Front-End Developer, I possess an impressive arsenal of skills in HTML, CSS, JavaScript, React, Tailwind, and SCSS. I excel in designing and maintaining responsive websites that offer a smooth user experience. My expertise lies in crafting dynamic, engaging interfaces through writing clean and optimized code and utilizing cutting-edge development tools and techniques. I am also a team player who thrives in collaborating with cross-functional teams to produce outstanding web applications.</p>
+      </div>
       </div>
     </section>
   )
--- src/Containers/Navbar/Nav.js
+++ src/Containers/Navbar/Nav.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import './nav.css';
+import React, { useState, useEffect } from "react";
+import "./nav.css";
 
 export default function Nav() {
   const [toggle, setToggle] = useState(false);
@@ -14,38 +14,55 @@ export default function Nav() {
       setPrevScrollPos(currentScrollPos);
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [prevScrollPos]);
 
   return (
-    <nav className={`navbar desktop ${visible ? '' : 'hidden'}`}>
-      <h4 className='nav-logo' >webdhan</h4>
-      <div className="nav-list">
-        <a href="#Home">Home</a>
-        <a href="#About">About</a>
-        <a href="#Project">Project</a>
-        <a href="#Contact">Contact</a>
-      </div>
-      <div className="mobile-view">
-        {toggle ?
-          <i className="fa-solid fa-xmark humberger" style={{ zIndex: "99" }} onClick={() => setToggle(false)} />
-          : <i className="fa-solid fa-bars humberger" style={{ zIndex: "99" }} onClick={() => setToggle(true)} />
-        }
-        {
-          toggle && (
+    <header>
+      <nav className={`navbar desktop ${visible ? "" : "hidden"}`}>
+        <h4 className="nav-logo">Webdhan</h4>
+        <div className="nav-list">
+          <a href="#Home">Home</a>
+          <a href="#About">About</a>
+          <a href="#Project">Project</a>
+          <a href="#Contact">Contact</a>
+        </div>
+        <div className="mobile-view">
+          {toggle ? (
+            <i
+              className="fa-solid fa-xmark humberger"
+              style={{ zIndex: "99" }}
+              onClick={() => setToggle(false)}
+            />
+          ) : (
+            <i
+              className="fa-solid fa-bars humberger"
+              style={{ zIndex: "99" }}
+              onClick={() => setToggle(true)}
+            />
+          )}
+          {toggle && (
             <div className="white scale-up-center">
-              <a href="#Home" onClick={() => setToggle(false)}>Home</a>
-              <a href="#About" onClick={() => setToggle(false)}>About</a>
-              <a href="#Project" onClick={() => setToggle(false)}>Project</a>
-              <a href="#Contact" onClick={() => setToggle(false)}>Contact</a>
+              <a href="#Home" onClick={() => setToggle(false)}>
+                Home
+              </a>
+              <a href="#About" onClick={() => setToggle(false)}>
+                About
+              </a>
+              <a href="#Project" onClick={() => setToggle(false)}>
+                Project
+              </a>
+              <a href="#Contact" onClick={() => setToggle(false)}>
+                Contact
+              </a>
             </div>
-          )
-        }
-      </div>
-    </nav>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 }
