6c04d6c196f27564ff4b4ebb12a202c6d2b721b1
dhanji-kumar <kdhanji183@gmail.com>
2024-05-03 20:18:31 +0530

    delete : clean all code and recreate with vite

--- src/Containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from '../Containers/Home/Home';
-export { default as Nav } from '../Containers/Navbar/Nav';
-export { default as About } from '../Containers/About/About';
-export { default as Contact } from '../Containers/Contact/Contact';
-export { default as Project } from '../Containers/Project/Project';
