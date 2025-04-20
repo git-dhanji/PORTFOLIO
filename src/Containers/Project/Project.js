acbe1146c324d2e5bdb83ffd14597a0171ac7953
dhanJi_exp <kdhanji183@gmail.com>
2025-04-20 11:53:58 +0530

    update project section

--- src/Containers/Project/Project.js
+++ src/Containers/Project/Project.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import './project.css'
-import {about2} from "../../assets/imports"
+
+
+import first from '../../assets/first.png'
+import three from '../../assets/three.png'
 
 
 export default function Project() {
@@ -15,14 +18,14 @@ export default function Project() {
       <div className="project-box">
         <div className="project-grid pro">
           <div className="project-image-grid">
-            <img src={about2} alt="" />
+            <img src={first} alt="" />
           </div>
           <div className="project-text-desc">
-            <h3>Project Name </h3>
-            <p>A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.</p>
+            <h3>Chai X blog </h3>
+            <p>Chai x Blog is a dynamic blogging platform built using the MERN stack (MongoDB, Express.js, React, and Node.js). It allows users to create, read, update, and delete blog posts in a clean and responsive interface.</p>
             <div className="project-livedemo">
-              <p><a href="https://github.com/pankaj-coder47">Code <i className="fa-brands fa-github"></i></a></p>
-              <p><a href="/">Livedemo <i className="fa-solid fa-arrow-up-right-from-square"></i></a></p>
+              <p><a href="https://github.com/git-dhanji/chaiXblog">Code <i className="fa-brands fa-github"></i></a></p>
+              <p><a href="https://chaixblog.vercel.app/">Livedemo <i className="fa-solid fa-arrow-up-right-from-square"></i></a></p>
 
             </div>
             <div className="project-text-btn">
@@ -40,8 +43,8 @@ export default function Project() {
             <h3>Project Name </h3>
             <p>A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars.</p>
             <div className="project-livedemo">
-              <p><a href="https://github.com/pankaj-coder47">Code <i className="fa-brands fa-github"></i></a></p>
-              <p><a href="/">Livedemo <i className="fa-solid fa-arrow-up-right-from-square"></i></a></p>
+              <p><a href="https://github.com/git-dhanji/Newswebsite_based_onApi">Code <i className="fa-brands fa-github"></i></a></p>
+              <p><a href="https://thenewsproject.vercel.app/">Livedemo <i className="fa-solid fa-arrow-up-right-from-square"></i></a></p>
 
             </div>
             <div className="project-text-btn">
@@ -50,7 +53,7 @@ export default function Project() {
             </div>
           </div>
           <div className="project-image-grid">
-            <img src={about2} alt="" />
+            <img src={three} alt="" />
           </div>
         </div>
       </div>
